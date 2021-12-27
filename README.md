ee1edc80ade9e016b543f57f4fdaccb63bbb07dd
textcunma <tiinyake@outlook.jp>
2021-12-27 12:56:15 +0900

    init

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # HumanDetect-WithYOLOV5
 YOLOV5を用いた人間検知
+
+**input: MP4 file** <br>
+**output: result.csv**
+
+YOLO V5を用いて人間を検知し以下の情報をCSVに書き込みます。
+- 人間が登場したフレーム番号
+- 信頼度スコア
+
+## SET UP
+```
+git clone --recursive git@github.com:textcunma/HumanDetect-WithYOLOV5.git
+```
+
+```
+conda create -n yolov5 python=3.8
+conda activate yolov5
+pip install -U -r yolov5/requirements.txt
+pip install pyqt5
+pip install opencv-python-headless
+```
---
+++ main.py
@@ -0,0 +1,23 @@
+"""
+入力：MP4ファイル
+出力：人間を検知したフレーム数
+"""
+import csv
+from detect import detect_main
+
+def main():
+    MP4path="./women_long_jump.mp4"      #MP4ファイルのパス名
+    conf=0.80       #信頼度確率（値以上）
+    result=detect_main(MP4path,conf)
+
+    with open('result.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(["frame","object","Conf Score"])
+        for r in result:
+            frame=r[0]
+            object,score=r[1].split()
+            if object=="person":
+                writer.writerow([frame,object,score])
+
+if __name__ == "__main__":
+    main()
